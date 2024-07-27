3a1cd657ffdbd6cad34c4c5023efa65cc4988c40
tobiloba14 <tobilobaogunleye14@gmail.com>
2024-07-26 23:00:57 -0400

    finish navbar

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-' use strict';
-
+'use strict';
 
 const preloader = document.querySelector("[data-preload]");
 
 window.addEventListener("load", function() {
     preloader.classList.add("loaded");
-    document.body.classList.add("loaded")
+    document.body.classList.add("loaded");
 });
+
+/**
+ * Add event listener on multiple elements
+ */
+const addEventOnElements = function(elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+};
+
+/**
+ * Navbar
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
